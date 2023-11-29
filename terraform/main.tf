447bec374af40d97662b082c0dc9311082e2386b
Irakli_Bakhtadze <irakli.bakhtadze@flowhealth.com>
2023-11-29 14:05:31 +0400

    identity provider creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 locals {
   prefix            = "TF-"  #Prefix for Roles & Policies
-  oidc_provider_arn = "arn:aws:iam::${var.aws_account}:oidc-provider/token.actions.githubusercontent.com" 
+  #oidc_provider_arn = "arn:aws:iam::${var.aws_account}:oidc-provider/token.actions.githubusercontent.com" 
   region = var.aws_region
   resource = "ec2"
   github_organization = "asrhayader"
@@ -16,10 +16,20 @@ locals {
   ]
 }
 
-#Github OIDC connection provider
-data "aws_iam_openid_connect_provider" "github" {
-  arn = local.oidc_provider_arn
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
 }
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  client_id_list  = ["sts.amazonaws.com"]
+}
+
+#Github OIDC connection provider
+#data "aws_iam_openid_connect_provider" "github" {
+#  arn = local.oidc_provider_arn
+#}
 
 #Policy document for github integration
 data "aws_iam_policy_document" "github_trust" {
@@ -28,7 +38,8 @@ data "aws_iam_policy_document" "github_trust" {
 
     principals {
       type        = "Federated"
-      identifiers = [local.oidc_provider_arn]
+      #identifiers = [local.oidc_provider_arn]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -47,11 +58,13 @@ data "aws_iam_policy_document" "github_trust" {
   }
 }
 
+#AWS role for github integration
 resource "aws_iam_role" "github" {
   name               = "${local.prefix}github-${local.github_repo}"
   assume_role_policy = data.aws_iam_policy_document.github_trust.json
 }
 
+#Policy document for EC2 management
 data "aws_iam_policy_document" "github" {
   dynamic "statement" {
     for_each = local.statements
@@ -64,11 +77,13 @@ data "aws_iam_policy_document" "github" {
   }
 }
 
+#AWS Policy generated from Policy Document
 resource "aws_iam_policy" "github" {
   name   = "${local.prefix}github-allow-access-to-${local.resource}-from-${local.github_repo}"
   policy = data.aws_iam_policy_document.github.json
 }
 
+#Attach polocy to AWS role
 resource "aws_iam_role_policy_attachment" "github" {
   role       = aws_iam_role.github.name
   policy_arn = aws_iam_policy.github.arn
